4d79410c8c2aac504fff3fa978d9bd569a12df91
Laz <laz@email.com>
2023-03-25 12:07:48 -0400

    Implement storehandler and test

--- src/init.lua
+++ src/init.lua
@@ -1,2 +1,25 @@
-local Packages = script.Parent.Parent
+--!strict
+local Packages = script.Parent
 local Fusion = require(Packages.Fusion)
+local Rodux = require(Packages.Rodux)
+
+local Value = Fusion.Value
+
+type State = {[string]: any}
+type StoreProcessor<T> = (State, State) -> T
+
+local function StoreHandler<T>(store: Rodux.Store, processor: (state, state) -> T)
+    local value: Fusion.Value<T> = Value(processor(store:getState()))
+
+    local connection = store.changed:connect(function(newState, oldState) 
+        local result = processor(newState, oldState)
+        value:set(result)
+    end)
+
+    local disconnect = function()
+        connection:disconnect()
+    end
+    return value, disconnect
+end
+
+return StoreHandler
---
+++ tests/test.story.lua
@@ -0,0 +1,93 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Fusion = require(ReplicatedStorage.Packages.Fusion)
+local Rodux: Rodux = require(ReplicatedStorage.Packages.Rodux)
+local StoreHandler = require(ReplicatedStorage.Packages.FusionRodux)
+
+local New = Fusion.New
+local Children = Fusion.Children
+local Cleanup = Fusion.Cleanup
+local Computed = Fusion.Computed
+local OnEvent = Fusion.OnEvent
+local doNothing = Fusion.doNothing
+
+local nameReducer = Rodux.createReducer("No Name", {
+    setName = function(_state, action)
+        print("Dispatched setName")
+        return action.name
+    end
+})
+
+local breedReducer = Rodux.createReducer("Unknown", {
+    setBreed = function(_state, action)
+        print("Dispatched setBreed")
+        return action.breed
+    end
+})
+
+local catReducer = Rodux.combineReducers({
+    name = nameReducer,
+    breed = breedReducer
+})
+
+local actions = {
+    setBreed = function(breed)
+        return {
+            type = "setBreed",
+            breed = breed
+        }
+    end,
+    setName = function(name)
+        return {
+            type = "setName",
+            name = name
+        }
+    end
+}
+
+local function Test(props)
+    local store = props.Store
+
+    local value, disconnect = StoreHandler(store, function(newState)
+        return newState
+    end)
+    return New "Frame" {
+        Size = UDim2.fromOffset(200, 200),
+        [Cleanup] = function()
+            print("Cleaning up...")
+            disconnect()
+        end,
+        [Children] = {
+            New "TextButton" {
+                Size = UDim2.fromOffset(200, 100),
+                Text = Computed(function()
+                    return value:get().name
+                end, doNothing),
+                [OnEvent "Activated"] = function()
+                    store:dispatch(actions.setName("Fanta"))
+                end
+            },
+            New "TextButton" {
+                Size = UDim2.fromOffset(200, 100),
+                Position = UDim2.fromOffset(0, 100),
+                Text = Computed(function()
+                    return value:get().breed
+                end, doNothing),
+                [OnEvent "Activated"] = function()
+                    store:dispatch(actions.setBreed("Orange Tabby"))
+                end
+            }
+        }
+    }
+end
+
+return function(screen)
+    local store = Rodux.Store.new(catReducer)
+    local test = Test {
+        Store = store
+    }
+    test.Parent = screen
+
+    return function()
+        test:Destroy()
+    end
+end
